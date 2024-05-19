760e1f3d81b3d38f922cb837c1a6148572ea668e
vlaszdunov <design@formo.design>
2024-05-19 17:02:57 +0300

    list of groups has became dictionary of groups

--- src/dataparser.py
+++ src/dataparser.py
@@ -1,6 +1,8 @@
-import pandas as pd
 from io import BytesIO
+from typing import Any
+
 import numpy as np
+import pandas as pd
 
 
 # <---------Creating list of groups--------->
@@ -46,7 +48,7 @@ def format_groups_data(file_content: bytes) -> pd.DataFrame:
     return all_data
 
 
-def create_list_of_groups(file_content: bytes) -> list[pd.DataFrame]:
+def create_list_of_groups(file_content: bytes) -> dict[str, dict[str, Any]]:
     '''
     Split pd.Dataframe to several DataFrames
     for each study groups and combine them to list
@@ -60,23 +62,18 @@ def create_list_of_groups(file_content: bytes) -> list[pd.DataFrame]:
 
     all_data = format_groups_data(file_content)
     mask_of_empty_cells = all_data.index.get_loc(0)
-    zero_indexes: list[int] = list(np.where(mask_of_empty_cells == True)[0])[:-1]
+    zero_indexes: list[int] = list(
+        np.where(mask_of_empty_cells == True)[0])[:-1]
     '''List of study group's divider's indexes'''
 
-    dict_of_groups: dict[str,pd.DataFrame]={}
-    all_data.insert(len(all_data.columns),'count',1)
-    print(all_data)
-    # for i in range(len(zero_indexes)-1):
-    #     dict_of_groups[f'Группа {zero_indexes[i]+1}'] = all_data[zero_indexes[i]+1:zero_indexes[i+1]].astype(float).assign(count=[1 for i in range(all_data.count())])
-    # list_of_groups: list[pd.DataFrame] = [all_data[zero_indexes[i]+1:zero_indexes[i+1]]
-    #                                       .astype(float) for i in range(len(zero_indexes)-2)]
-    # print(dict_of_groups)
-    exit()
+    dict_of_groups: dict[str, dict[str, Any]] = {}
+    for i in range(len(zero_indexes)-1):
+        dict_of_groups[f'Группа {i+1}'] = {
+            'group_list': all_data[zero_indexes[i]+1:zero_indexes[i+1]].astype(float)}
+        dict_of_groups[f'Группа {i+1}']\
+            .update({'count': zero_indexes[i+1] - zero_indexes[i]-1})
+
     '''Create list of study groups'''
 
-    # list_of_groups.append(pd.concat(list_of_groups, axis=0)
-    #                       .set_axis([f'Группа {i}' for i in range(1, len(list_of_groups)+1)],
-    #                                 axis='index'))
-    # '''Appending pd.Dataframe with all study groups to group's list'''
-    return list_of_groups
+    return dict_of_groups
 # <----------------------------------------->
--- src/visualiser.py
+++ src/visualiser.py
@@ -1,33 +1,36 @@
 import os
+from typing import Any
 
 import matplotlib.pyplot as plt
-import seaborn
 import pandas as pd
+import seaborn
 
 
-def create_heatmap(dataset: list[pd.DataFrame]):
+def create_heatmap(dataset: dict[str, dict[str, Any]]):
     '''
     Creating and saving heatmaps as pictores
-    
+
     Args:
         dataset (list): list of study group's pd.DataFrames
     '''
-    
+
     if os.path.exists('exported') == False:
         os.mkdir('exported')
-        
-    for group in range(len(dataset)):
-        if group==len(dataset)-1:
-            seaborn.heatmap(dataset[group].iloc[:,:-1], annot=True, cbar=False,linewidths=0.5)
-            '''
-            Generating heatmap for dataset[group] without last column,
-            that contains 1'st ex score
-            '''
-            
-            plt.title('Посещаемость всех групп')
-            plt.savefig('exported/'+'all_groups.png')
-        else:
-            plt.title(f'Посещаемость группы {group+1}')
-            seaborn.heatmap(dataset[group].iloc[:,:-1], annot=True, cbar=False,linewidths=0.5)
-            plt.savefig('exported/'+f'group {group+1}.png')
+
+    for group in dataset.keys():
+        # if dataset[group] == len(dataset)-1:
+        #     seaborn.heatmap(dataset[group]['group_list'].iloc[:, :-1],
+        #                     annot=True, cbar=False, linewidths=0.5)
+        #     '''
+        #     Generating heatmap for dataset[group] without last column,
+        #     that contains 1'st ex score
+        #     '''
+
+        #     plt.title('Посещаемость всех групп')
+        #     plt.savefig('exported/'+'all_groups.png')
+
+        plt.title(f'Посещаемость группы {group}')
+        seaborn.heatmap(dataset[group]['group_list'].iloc[:, :-1],
+                        annot=True, cbar=False, linewidths=0.5)
+        plt.savefig('exported/'+f'{group}.png')
         plt.cla()
